8679987abe5ec41e5bedffee51f1849ab02f340a
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-03-22 12:26:48 +0530

    ADD: Calculated Employees Daily Wage

--- empWage.java
+++ empWage.java
@@ -4,12 +4,15 @@ public class empWage {
 
 	public static void main(String[] args){
 		System.out.println("Welcome to Employee Wage Computation");
+		int wagePerHr = 20;
+		int fullDayHrs = 8;
 		Random check = new Random();
 		int empCheck = check.nextInt(2);
 		if(empCheck == 1){
-			System.out.println("Employee Present");
+			int dailyWage = wagePerHr * fullDayHrs;
+			System.out.println( dailyWage + " is the employees wage per day");
 		}else{
-			System.out.println("Employee Absent");
+			System.out.println("Employee is Absent");
 		}
 	}
 }
