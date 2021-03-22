0cc69a654c47c2e2beb81867da33cf05f1f2ae37
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-03-22 13:06:51 +0530

    Modified: using switch case

--- empWage.java
+++ empWage.java
@@ -7,19 +7,24 @@ public class empWage {
 		int wagePerHr = 20;
 		int fullTimeHrs = 8;
 		int partTimeHrs = 4;
+		int dailyWage = 0;
 		Random check = new Random();
 		int empCheck = check.nextInt(3);
-		System.out.println(empCheck);
-		if(empCheck == 2){
-			int dailyWage = wagePerHr * fullTimeHrs;
-			System.out.println( dailyWage + " is the employees full time wage per day");
-		}else {
-			if(empCheck == 1){
-				int dailyWage = wagePerHr * partTimeHrs;
-				System.out.println( dailyWage + " is the employees part time wage per day");
-			}else{
-			System.out.println("Employee is Absent");
-			}
+		switch(empCheck) {
+		case 0:
+				System.out.println("Employee is absent");
+				break;
+		case 1:
+				dailyWage = wagePerHr * partTimeHrs;
+            System.out.println( dailyWage + " is the employees part time wage per day");
+				break;
+		case 2:
+				dailyWage = wagePerHr * fullTimeHrs;
+				System.out.println( dailyWage + " is the employees full time wage per day");
+				break;
+		default:
+				System.out.println("invalid");
+				break;
 		}
 	}
 }
