58eee38aecea8b3c407acc37b4deda4ef09d1ee7
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-03-22 13:15:44 +0530

    ADD: calculated wages for 20 days

--- empWage.java
+++ empWage.java
@@ -8,23 +8,27 @@ public class empWage {
 		int fullTimeHrs = 8;
 		int partTimeHrs = 4;
 		int dailyWage = 0;
-		Random check = new Random();
-		int empCheck = check.nextInt(3);
-		switch(empCheck) {
-		case 0:
-				System.out.println("Employee is absent");
-				break;
-		case 1:
-				dailyWage = wagePerHr * partTimeHrs;
-            System.out.println( dailyWage + " is the employees part time wage per day");
-				break;
-		case 2:
-				dailyWage = wagePerHr * fullTimeHrs;
-				System.out.println( dailyWage + " is the employees full time wage per day");
-				break;
-		default:
-				System.out.println("invalid");
-				break;
+		int totalWorkingDays = 20;
+		for( int day = 1; day<= totalWorkingDays; day++){
+
+			Random check = new Random();
+			int empCheck = check.nextInt(3);
+			switch(empCheck) {
+			case 0:
+					System.out.println("Employee is absent");
+					break;
+			case 1:
+					dailyWage = wagePerHr * partTimeHrs;
+      	      System.out.println( dailyWage + " is the employees part time wage per day");
+					break;
+			case 2:
+					dailyWage = wagePerHr * fullTimeHrs;
+					System.out.println( dailyWage + " is the employees full time wage per day");
+					break;
+			default:
+					System.out.println("invalid");
+					break;
+			}
 		}
 	}
 }
