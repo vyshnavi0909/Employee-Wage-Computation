bb047842c422716f22a9fb676f40e516328219c1
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-03-22 12:59:23 +0530

    ADD: Part time wage per day

--- empWage.java
+++ empWage.java
@@ -5,14 +5,21 @@ public class empWage {
 	public static void main(String[] args){
 		System.out.println("Welcome to Employee Wage Computation");
 		int wagePerHr = 20;
-		int fullDayHrs = 8;
+		int fullTimeHrs = 8;
+		int partTimeHrs = 4;
 		Random check = new Random();
-		int empCheck = check.nextInt(2);
-		if(empCheck == 1){
-			int dailyWage = wagePerHr * fullDayHrs;
-			System.out.println( dailyWage + " is the employees wage per day");
-		}else{
+		int empCheck = check.nextInt(3);
+		System.out.println(empCheck);
+		if(empCheck == 2){
+			int dailyWage = wagePerHr * fullTimeHrs;
+			System.out.println( dailyWage + " is the employees full time wage per day");
+		}else {
+			if(empCheck == 1){
+				int dailyWage = wagePerHr * partTimeHrs;
+				System.out.println( dailyWage + " is the employees part time wage per day");
+			}else{
 			System.out.println("Employee is Absent");
+			}
 		}
 	}
 }
