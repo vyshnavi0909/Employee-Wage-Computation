d20fe65a0016ff76584f1fea2dd333ed7438e236
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-03-22 12:17:23 +0530

    ADD: Employee attendance

--- empWage.java
+++ empWage.java
@@ -1,7 +1,15 @@
+import java.util.*;
+
 public class empWage {
 
 	public static void main(String[] args){
-
 		System.out.println("Welcome to Employee Wage Computation");
+		Random check = new Random();
+		int empCheck = check.nextInt(2);
+		if(empCheck == 1){
+			System.out.println("Employee Present");
+		}else{
+			System.out.println("Employee Absent");
+		}
 	}
 }
